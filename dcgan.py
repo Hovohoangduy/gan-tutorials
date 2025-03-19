30fffd4d5fae730e826abf01e23a5d615626e9f6
duyho <h.hoangduy2002@gmail.com>
2025-03-20 01:14:43 +0700

    update

--- dcgan.py
+++ dcgan.py
@@ -3,12 +3,8 @@ import os
 import random
 import torch
 import torch.nn as nn
-import torch.nn.parallel
-import torch.optim as optim
-import torch.utils.data
-import torchvision.datasets as dset
+import torchvision
 import torchvision.transforms as transforms
-import torchvision.utils as vutils
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -18,7 +14,6 @@ random.seed(1105)
 torch.manual_seed(1105)
 torch.use_deterministic_algorithms(True)
 
-dataroot = 'data/celebs'
 workers = 2 # number of worker for dataloader
 batch_size = 128
 image_size = 64
@@ -26,18 +21,201 @@ nc = 3 # number of channels in the images
 nz = 10 # size of z latent vector (size of generator input)
 ngf = 64 # size of feature maps in generator
 ndf = 64 # size of feature maps in discriminator
-num_epochs = 5
+num_epochs = 10
 lr = 0.0002
 beta1 = 0.5 # beta1 hyperparameter for Adam optimizers
 ngpu = 1 # number of GPUs availiable
 
-dataset = dset.ImageFolder(root=dataroot,
-                           transform=transforms)
 transform = transforms.Compose([
     transforms.Resize(image_size),
     transforms.CenterCrop(image_size),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cup")
+trainset = torchvision.datasets.CIFAR10(
+    root=".", train=True, download=True, transform=transform
+)
+testset = torchvision.datasets.CIFAR10(
+    root=".", train=True, download=True, transform=transform
+)
+
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=workers)
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+real_batch = next(iter(trainset))
+# plt.figure(figsize=(8,8))
+# plt.axis("off")
+# plt.title("Training Images")
+# plt.imshow(np.transpose(torchvision.utils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1,2,0)))
+# plt.show()
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != - 1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super().__init__()
+        self.mgpu = ngpu
+        self.main = nn.Sequential(
+            # input is z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. ``(ngf*8) x 4 x 4``
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. ``(ngf*4) x 8 x 8``
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # state size. ``(ngf*2) x 16 x 16``
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. ``(ngf) x 32 x 32``
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. ``(nc) x 64 x 64``
+        )
+    def forward(self, input):
+        return self.main(input)
+    
+netG = Generator(ngpu).to(device)
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+netG.apply(weights_init)
+print("NetG: ", netG)
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            # input is ``(nc) x 64 x 64``
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ndf) x 32 x 32``
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ndf*2) x 16 x 16``
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ndf*4) x 8 x 8``
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. ``(ndf*8) x 4 x 4``
+            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+        
+netD = Discriminator(ngpu).to(device)
+if (device.type == 'cuda') and (ngpu > 1):
+    netD = nn.DataParallel(netD, list(range(ngpu)))
+netD.apply(weights_init)
+print("NetD: ", netD)
+
+
+criterion = nn.BCELoss()
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+# establish convention for real and fake labels during training
+real_label = 1.
+fake_label = 0.
+optimizerD = torch.optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = torch.optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
+# training
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Staring training loop...")
+for epoch in range(num_epochs):
+    for i, data in enumerate(train_loader, 0):
+        # (1) update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        netD.zero_grad()
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+        output = netD(real_cpu).view(-1)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.mean().item()
+        ## train with all-fake batch
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # generate fake image batch with G
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # classify all fake batch with D
+        output = netD(fake.detach()).view(-1)
+        errD_fake = criterion(output, label)
+        # calculate the gradients for this batch, accumulated (summed) with previous gradients
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # compute error of D as sum over the fake and the real batches
+        errD = errD_real + errD_fake
+        # update D
+        optimizerD.step()
+        # (2) update G network: maximize log(D(G(z)))
+        netG.zero_grad()
+        label.fill_(fake_label)
+        output = netD(fake).view(-1)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        # update G
+        optimizerG.step()
+        # output the training stats
+        if i % 50 == 0:
+            print('[%d/%d] [%d/%d] \t Loss_D: %.4f \t Loss_G: %.4f \t D(x): %.4f \t D(G(z)): %.4f / %.4f' 
+                  % (epoch, num_epochs, i, len(train_loader), errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+        # checking how the generator is doing by saving G's output on fixed noise
+        if (iters % 500 == 0) or ((epoch == num_epochs - 1) and (i == len(train_loader) - 1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            img_list.append(torchvision.utils.make_grid(fake, padding=2, normalize=True))
+        iters += 1
+
+# plot result
+os.makedirs("tmp", exist_ok=True)
+
+plt.figure(figsize=(10, 5))
+plt.title("Generator and Dicriminator loss during training")
+plt.plot(G_losses, label="G")
+plt.plot(D_losses, label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.savefig("tmp/plot_result.jpg")
+plt.show()
+
+# visualize real images and fake images
+real_batch = next(iter(train_loader))
+plt.figure(figsize=(15, 15))
+plt.subplot(1, 2, 1)
+plt.axis("off")
+plt.title("real images")
+plt.imshow(np.transpose(torchvision.utils.make_grid(real_batch[0].to(device)[:64], 
+                                                    padding=5, normalize=True).cpu(), (1, 2, 0)))
+
+plt.subplot(1, 2, 2)
+plt.axis("off")
+plt.title("fake images")
+plt.imshow(np.transpose(img_list[-1], (1, 2, 0)))
+plt.savefig("tmp/real_fake_images.jpg")
+plt.show()
